d7749d4407ad7a64057ef73571b6e028cd04edb9
akurfurst <akurfurst01@gamil.com>
2026-02-11 19:58:58 -0800

    Answer q2

--- q2.js
+++ q2.js
@@ -1,9 +1,14 @@
 // 2. Find the largest number in an array (using a loop)
 function findLargest(array) {
-
+    let result = 0;
+    for(let i = 0; i < array.length; i++){
+        if(array[i] > result) result = array[i];
+    }
+    return result;
 }
 
 // Test for find largest
 let testArray2 = [1, 4, 5, 4, 3]; // 5
 
 // TODO: display the largest number in the array in the console
+console.log(findLargest(testArray2));
